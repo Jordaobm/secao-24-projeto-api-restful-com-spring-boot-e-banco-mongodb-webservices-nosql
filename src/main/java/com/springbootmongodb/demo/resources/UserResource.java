be1531d19ff38cb4c49928f1b52ec1d323db4fb2
Jordão Beghetto Massariol <jbmassariol@gmail.com>
2024-05-16 15:26:34 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/springbootmongodb/demo/resources/UserResource.java
+++ src/main/java/com/springbootmongodb/demo/resources/UserResource.java
@@ -1,25 +1,29 @@
 package com.springbootmongodb.demo.resources;
 
 import com.springbootmongodb.demo.entities.User;
+import com.springbootmongodb.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping()
-    public ResponseEntity<List<User>> create() {
-        User a = new User("1", "John Doe 1", "johndoe1@example.com");
-        User b = new User("2", "John Doe 2", "johndoe2@example.com");
-        List<User> list = new ArrayList<User>();
-        list.addAll(Arrays.asList(a, b));
-        return ResponseEntity.ok().body(list);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> create(@RequestBody User user) {
+        User newUser = userService.create(user);
+        return ResponseEntity.ok().body(newUser);
     }
 }
--- src/main/java/com/springbootmongodb/demo/services/UserService.java
+++ src/main/java/com/springbootmongodb/demo/services/UserService.java
@@ -1,4 +1,30 @@
 package com.springbootmongodb.demo.services;
 
+import com.springbootmongodb.demo.entities.User;
+import com.springbootmongodb.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User create(User user) {
+        User newUser = new User();
+
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+
+        userRepository.save(newUser);
+        return newUser;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
 }
